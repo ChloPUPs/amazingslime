2ca3ebfb3efb1e967f55ad56b42ec64225d36453
ElectricalOutlet <alexplaysgamezzz@outlook.com>
2026-06-04 06:41:21 -0700

    Left, right.

--- main.py
+++ main.py
@@ -8,16 +8,21 @@ class Player:
         self.dest = pg.FRect(20.0, 20.0, 16.0, 16.0)
 
 
+def get_movement(input_state):
+    return input_state.events["right"].held - input_state.events["left"].held
+
+
 def run():
     pg.init()
 
+    FPS_TARGET = 60
+
     screen = pg.display.set_mode((640, 480))
     display = pg.Surface((screen.width / 2, screen.height / 2))
+    clock = pg.Clock()
     input_state = input_sys.InputState()
 
     player = Player()
-
-    num = 1
 
     while True:
         input_state.update_just_pressed()
@@ -27,13 +32,13 @@ def run():
                 sys.exit()
             input_state.update_input(event)
 
-        if input_state.events["up"].held:
-            num += 1
-            print(f"Apple sauce! x{num}...")
+        player.dest.x += get_movement(input_state)
 
+        display.fill("black")
         pg.draw.rect(display, "lime", player.dest)
 
         screen.blit(pg.transform.scale(display, screen.get_size()))
+        clock.tick(FPS_TARGET)
         pg.display.update()
 
 
